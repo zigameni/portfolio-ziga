6f7cb73a22e11920ffcfb61a1e3a708003c4ee3c
Ziga <shehu.meni@gmail.com>
2024-09-29 02:22:12 +0200

    Make a few changes to the styling

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
-import Globe from "react-globe.gl";
-import Button from "../components/Button.jsx";
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
+
+import Button from '../components/Button.jsx';
 
 const About = () => {
     const [hasCopied, setHasCopied] = useState(false);
@@ -15,10 +16,10 @@ const About = () => {
     };
 
 
-    return (
-        <section className="c-space my-20">
-            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
 
+    return (
+        <section className="c-space my-20" id="about">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
@@ -26,7 +27,8 @@ const About = () => {
                         <div>
                             <p className="grid-headtext">Hi, I'm Gazi</p>
                             <p className="grid-subtext">
-                                With years of experience, I have honed my skills in both frontend and backend development, creating dynamic and responsive websites.
+                                With years of experience, I have honed my skills in both frontend and backend
+                                development, creating dynamic and responsive websites.
                             </p>
 
                         </div>
@@ -36,9 +38,14 @@ const About = () => {
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
+
                         <div>
                             <p className="grid-headtext">Tech Stack</p>
-                            <p className="grid-subtext"> I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable applications</p>
+                            <p className="grid-subtext">
+                                I specialize in a variety of languages, frameworks, and tools that allow me to build
+                                robust and scalable
+                                applications
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -68,15 +75,15 @@ const About = () => {
                 </div>
 
 
-                <div className="col-span-2 xl:row-span-3">
+                <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
                         <div>
                             <p className="grid-headtext">My Passion for Coding</p>
                             <p className="grid-subtext">
                                 I love solving problems and building things through code. Programming isn&apos;t just my
-                                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my
-                                skills.
+                                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
                             </p>
                         </div>
                     </div>
@@ -84,20 +91,24 @@ const About = () => {
 
                 <div className="xl:col-span-1 xl:row-span-2">
                     <div className="grid-container">
-                        <img src="/assets/grid4.png" alt="grid-4" className="w-full sm:h-[126px] sm:h-[276px h-fit object-cover sm:object-top"/>
+                        <img
+                            src="assets/grid4.png"
+                            alt="grid-4"
+                            className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+                        />
+
                         <div className="space-y-2">
                             <p className="grid-subtext text-center">Contact me</p>
                             <div className="copy-container" onClick={handleCopy}>
-                                <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy"/>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
                                 <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">shehu.meni@gmail.com</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </section>
-    )
-}
-export default About
+    );
+};
+
+export default About;
